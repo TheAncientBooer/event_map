4dde793d0cf303d7eb35ec53c2652326c945a8c3
randall <randall.mayton@gmail.com>
2022-10-02 11:41:39 -0700

    qs

--- map_project/map/forms.py
+++ map_project/map/forms.py
@@ -3,7 +3,7 @@ from .models import Event
 from django.forms import ModelForm
 
 class EventForm(forms.ModelForm):
-    #address = forms.CharField(label='')
+    address = forms.CharField(label='address')
     class Meta:
         model = Event
         fields = '__all__'
--- map_project/map/views.py
+++ map_project/map/views.py
@@ -24,23 +24,41 @@ def index(request):
     #     return HttpResponse('Please enter a valid address. Refresh the page to try again.')
 
     # Create Map Object
-    m = folium.Map(location=[lat, lng], zoom_start=1.5) #works
-    tooltip = "View Event!"
+    m = folium.Map(location=[45.5236, -122.6750], zoom_start=1.5) #works
+    #tooltip = "View Event!"
 
+    # Add Marker
+    folium.Marker(
+        location=[lat, lng],
+        popup=address,
+        icon=folium.Icon(color='red', icon='info-sign')
+    ).add_to(m)
+
+    m = m._repr_html_()
+
+    context = {
+        'form': form,
+        'm': m,
+        'address': address,
+        'lat': lat,
+        'lng': lng,
+        'country': country,
+    }
+    return render(request, 'index.html', context)
     #folium.Marker([45.3288, -121.6625], popup= title, tooltip=tooltip).add_to(m) #change tooltip to event title
     # folium.Marker([45.3311, -121.7113], popup="<i>Timberline Lodge</i>", tooltip=tooltip).add_to(m)
     # folium.Marker([33.41063301181929, -82.13471009798371], popup="<i>Annex</i>", tooltip=tooltip).add_to(m)
 
-    folium.Marker([lat, lng], tooltip='Click for more', popup=country).add_to(m)
-    # Get HTML Representation of Map Object
-    m = m._repr_html_()
-    context = {
-        'm': m,
-        'form': form,
-        #'address': address,
-    }
-    
-    return render(request, 'index.html', context)
+    # folium.Marker([lat, lng], tooltip='Click for more', popup=country).add_to(m)
+    # # Get HTML Representation of Map Object
+    # m = m._repr_html_()
+    # context = {
+    #     'm': m,
+    #     'form': form,
+    #     'address': address,
+    # }
+    # #m.save("index.html")
+    # return render(request, 'index.html', context)
             
 
 def post_event(request):
